6d3eaa7e20ccc1f62fcec20d45e459d03a813ea4
StudyMingle <pushan45hsh@gmail.com>
2024-10-24 16:45:44 +0900

    feat : bell 추가 및 코드 리펙토링

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { default as Button } from "./components/Button";
 export { default as ChevronDouble } from "./components/icons/ChevronDouble";
 export { default as Chevron } from "./components/icons/Chevron";
 export { default as ChevronTight } from "./components/icons/ChevronTight";
+export { default as Bell } from "./components/icons/IconBell";
