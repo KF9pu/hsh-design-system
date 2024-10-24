10401eb8908c22f8565bb3fa451d477101eabe88
StudyMingle <pushan45hsh@gmail.com>
2024-10-24 17:02:32 +0900

    feat :  BookMark 추가

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ export { default as ChevronDouble } from "./components/icons/ChevronDouble";
 export { default as Chevron } from "./components/icons/Chevron";
 export { default as ChevronTight } from "./components/icons/ChevronTight";
 export { default as Book } from "./components/icons/Book";
+export { default as BookMark } from "./components/icons/BookMark";
 export { default as Bell } from "./components/icons/Bell";
