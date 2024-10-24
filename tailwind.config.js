089151d58011789de72bcbb34dead5c71a56fbe2
StudyMingle <pushan45hsh@gmail.com>
2024-10-24 14:17:57 +0900

    feat : 모듈 스타일 적용 경로 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/hsh-design-system/dist/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
