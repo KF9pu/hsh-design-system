fdc9821878db8da0b7cff9b53aadf29b3d58b0b7
StudyMingle <pushan45hsh@gmail.com>
2024-10-24 16:53:54 +0900

    feat : Book 추가

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export { default as Button } from "./components/Button";
 export { default as ChevronDouble } from "./components/icons/ChevronDouble";
 export { default as Chevron } from "./components/icons/Chevron";
 export { default as ChevronTight } from "./components/icons/ChevronTight";
-export { default as Bell } from "./components/icons/IconBell";
+export { default as Book } from "./components/icons/Book";
+export { default as Bell } from "./components/icons/Bell";
