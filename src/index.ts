8a3b906958f20d8932c83bd86d3cc4b4ddbc2ab5
StudyMingle <pushan45hsh@gmail.com>
2024-10-24 15:57:00 +0900

    feat : ChevronRightDouble export 추가

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import "./style.css";
 export { default as Button } from "./components/Button";
 export { default as ChevronLeftDouble } from "./components/icons/ChevronLeftDouble";
+export { default as ChevronRightDouble } from "./components/icons/ChevronRightDouble";
