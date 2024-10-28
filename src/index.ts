57b7c92eba020aa8490a9f12d6964deab90a7901
StudyMingle <pushan45hsh@gmail.com>
2024-10-28 09:51:34 +0900

    chore : 미사용 코드 제거

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import "./style.css";
-export { default as Button } from "./components/Button";
 export { default as ChevronDouble } from "./components/icons/ChevronDouble";
 export { default as Chevron } from "./components/icons/Chevron";
 export { default as ChevronTight } from "./components/icons/ChevronTight";
