4a288e940b44ed2643b6165499793d9299bd5425
StudyMingle <pushan45hsh@gmail.com>
2024-10-29 15:51:51 +0900

    feat : color 팔레트 1차 저장

---
+++ src/components/forms/CheckBox.tsx
@@ -0,0 +1,13 @@
+import type { FC } from "react";
+import React from "react";
+
+interface CheckBoxProps {}
+
+const CheckBox: FC<CheckBoxProps> = ({}) => {
+  return (
+    <label>
+      <input type="text" />
+    </label>
+  );
+};
+export default CheckBox;
