faea2991b02b866897dc607bb0bf0745c484e397
StudyMingle <pushan45hsh@gmail.com>
2024-10-31 10:11:40 +0900

    fix : 인자 타입 수정

--- src/components/animationBoxs/LoadingFadeBubble.tsx
+++ src/components/animationBoxs/LoadingFadeBubble.tsx
@@ -4,14 +4,14 @@ import useDefaultLoading from "../../hooks/useDefaultLoading";
 import "../../theme.css";
 
 interface LoadingFadeBubbleProps {
-  FadeBubbleColor1: string;
-  FadeBubbleColor2: string;
-  FadeBubbleColor3: string;
-  FadeBubbleColor4: string;
+  FadeBubbleColor1?: string;
+  FadeBubbleColor2?: string;
+  FadeBubbleColor3?: string;
+  FadeBubbleColor4?: string;
 }
 
 const LoadingFadeBubble: FC<LoadingFadeBubbleProps> = ({
-  FadeBubbleColor1 = "007bff",
+  FadeBubbleColor1 = "#007bff",
   FadeBubbleColor2 = "rgba(173, 255, 47, 0.9)",
   FadeBubbleColor3 = "rgba(70, 130, 180, 0.9)",
   FadeBubbleColor4 = "rgba(135, 206, 250, 0.9)",
