0c6d7efed18a7de9d0fcb64c1d4b517649cbdbc8
StudyMingle <pushan45hsh@gmail.com>
2024-10-31 10:07:51 +0900

    fix : LoadingFadeBubble color 인자로 받을 수 있도록 수정

--- src/components/animationBoxs/LoadingFadeBubble.tsx
+++ src/components/animationBoxs/LoadingFadeBubble.tsx
@@ -3,15 +3,39 @@ import type { FC, HtmlHTMLAttributes } from "react";
 import useDefaultLoading from "../../hooks/useDefaultLoading";
 import "../../theme.css";
 
-const LoadingFadeBubble: FC<unknown> = () => {
+interface LoadingFadeBubbleProps {
+  FadeBubbleColor1: string;
+  FadeBubbleColor2: string;
+  FadeBubbleColor3: string;
+  FadeBubbleColor4: string;
+}
+
+const LoadingFadeBubble: FC<LoadingFadeBubbleProps> = ({
+  FadeBubbleColor1 = "007bff",
+  FadeBubbleColor2 = "rgba(173, 255, 47, 0.9)",
+  FadeBubbleColor3 = "rgba(70, 130, 180, 0.9)",
+  FadeBubbleColor4 = "rgba(135, 206, 250, 0.9)",
+}) => {
   const { isLoading } = useDefaultLoading(1500);
 
   return (
     <main className={`loading-container ${isLoading ? "" : "hidden"}`}>
-      <FadeBubble className="fade-bubble fade-bubble-primary" />
-      <FadeBubble className="fade-bubble fade-bubble-lime" />
-      <FadeBubble className="fade-bubble fade-bubble-blue" />
-      <FadeBubble className="fade-bubble fade-bubble-sky" />
+      <FadeBubble
+        className="fade-bubble fade-bubble-primary"
+        style={{ backgroundColor: FadeBubbleColor1 }}
+      />
+      <FadeBubble
+        className="fade-bubble fade-bubble-lime"
+        style={{ backgroundColor: FadeBubbleColor2 }}
+      />
+      <FadeBubble
+        className="fade-bubble fade-bubble-blue"
+        style={{ backgroundColor: FadeBubbleColor3 }}
+      />
+      <FadeBubble
+        className="fade-bubble fade-bubble-sky"
+        style={{ backgroundColor: FadeBubbleColor4 }}
+      />
       <div>Loading ... </div>
     </main>
   );
