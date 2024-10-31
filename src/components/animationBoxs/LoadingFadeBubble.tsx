1b1507dff2f41938b156c7d800b8163ee94fcb08
StudyMingle <pushan45hsh@gmail.com>
2024-10-31 10:29:27 +0900

    feat : 중앙 로딩 텍스트 컬러 인자추가, 및 변경 할수 있도록

--- src/components/animationBoxs/LoadingFadeBubble.tsx
+++ src/components/animationBoxs/LoadingFadeBubble.tsx
@@ -1,29 +1,33 @@
 import React from "react";
-import type { FC, HtmlHTMLAttributes } from "react";
+import type { FC, HtmlHTMLAttributes, ReactNode } from "react";
 import useDefaultLoading from "../../hooks/useDefaultLoading";
 import "../../theme.css";
 
 interface LoadingFadeBubbleProps {
-  maionColor?: string;
+  mainColor?: string;
+  defaultTextColor?: string;
   FadeBubbleColor1?: string;
   FadeBubbleColor2?: string;
   FadeBubbleColor3?: string;
   FadeBubbleColor4?: string;
+  children?: ReactNode;
 }
 
 const LoadingFadeBubble: FC<LoadingFadeBubbleProps> = ({
-  maionColor = "white",
+  mainColor = "white",
+  defaultTextColor = "black",
   FadeBubbleColor1 = "#007bff",
   FadeBubbleColor2 = "rgba(173, 255, 47, 0.9)",
   FadeBubbleColor3 = "rgba(70, 130, 180, 0.9)",
   FadeBubbleColor4 = "rgba(135, 206, 250, 0.9)",
+  children,
 }) => {
   const { isLoading } = useDefaultLoading(1500);
 
   return (
     <main
       className={`loading-container ${isLoading ? "" : "hidden"}`}
-      style={{ backgroundColor: maionColor }}
+      style={{ backgroundColor: mainColor }}
     >
       <FadeBubble
         className="fade-bubble fade-bubble-primary"
@@ -41,7 +45,7 @@ const LoadingFadeBubble: FC<LoadingFadeBubbleProps> = ({
         className="fade-bubble fade-bubble-sky"
         style={{ backgroundColor: FadeBubbleColor4 }}
       />
-      <div>Loading ... </div>
+      {children ?? <div style={{ color: defaultTextColor }}>Loading ... </div>}
     </main>
   );
 };
