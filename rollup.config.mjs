2fbf12386d3370891e37b0d66a8c5300b5d2f28e
StudyMingle <pushan45hsh@gmail.com>
2024-11-04 16:45:47 +0900

    feat : rollup 파일 복사 설정 추가

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,7 +4,7 @@ import typescript from "rollup-plugin-typescript2";
 import postcss from "rollup-plugin-postcss";
 import del from "rollup-plugin-delete";
 import path from "path";
-import url from "@rollup/plugin-url";
+import copy from "rollup-plugin-copy";
 
 export default {
   input: "src/index.ts", // 모듈의 진입점
@@ -30,11 +30,9 @@ export default {
     postcss({
       extract: path.resolve("dist/theme.css"),
     }),
-    url({
-      include: ["**/*.ttf"],
-      limit: 0, // 0으로 설정하면 폰트를 별도의 파일로 출력합니다.
-      emitFiles: true,
-      fileName: "assets/fonts/[name][extname]",
+    copy({
+      targets: [{ src: "fonts/*", dest: "dist/assets/fonts" }],
+      verbose: true, // 빌드 로그에 복사 과정을 표시
     }),
   ],
   external: ["react", "react-dom"], // 외부 의존성 제외
