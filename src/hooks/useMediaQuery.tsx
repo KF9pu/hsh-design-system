0ff3a1b0b1d68706d763654073830cd019a52066
StudyMingle <pushan45hsh@gmail.com>
2024-12-10 10:20:44 +0900

    fix : 미디어 쿼리 최적화
    - 브레이크 포인트 enum 업데이트로 사용방법 변경
    - 라이브러리 미반영

--- src/hooks/useMediaQuery.tsx
+++ src/hooks/useMediaQuery.tsx
@@ -1,42 +1,59 @@
 import { useEffect, useState } from "react";
+import { debounce } from "lodash";
+
+// TODO: 라이브러리 설치 후에도 적용 되도록 lodash 세팅 해야함
+
+// Define Breakpoint Enum
+export enum Breakpoint {
+  XS = "xs",
+  SM = "sm",
+  MD = "md",
+  LG = "lg",
+}
+
+const breakpoints = {
+  [Breakpoint.XS]: 768,
+  [Breakpoint.SM]: 992,
+  [Breakpoint.MD]: 1200,
+};
+
+const getBreakpoint = (width: number): Breakpoint => {
+  if (width < breakpoints[Breakpoint.XS]) return Breakpoint.XS;
+  if (width < breakpoints[Breakpoint.SM]) return Breakpoint.SM;
+  if (width < breakpoints[Breakpoint.MD]) return Breakpoint.MD;
+  return Breakpoint.LG;
+};
 
 const useMediaQuery = () => {
-  const [innerWidth, setInnerWidth] = useState(0);
-  const [innerHeight, setInnerHeight] = useState(0);
-  const [breakpoint, setBreakpoint] = useState("xs");
+  const [innerWidth, setInnerWidth] = useState(
+    typeof window !== "undefined" ? window.innerWidth : 0
+  );
+  const [innerHeight, setInnerHeight] = useState(
+    typeof window !== "undefined" ? window.innerHeight : 0
+  );
+  const [breakpoint, setBreakpoint] = useState(
+    typeof window !== "undefined"
+      ? getBreakpoint(window.innerWidth)
+      : Breakpoint.XS
+  );
 
   useEffect(() => {
     if (typeof window !== "undefined") {
-      const handleResize = () => {
+      const handleResize = debounce(() => {
         const width = window.innerWidth;
         setInnerWidth(width);
         setInnerHeight(window.innerHeight);
-
-        // Determine breakpoint based on width
-        if (width < 768) {
-          setBreakpoint("xs");
-        } else if (width >= 768 && width < 992) {
-          setBreakpoint("sm");
-        } else if (width >= 992 && width < 1200) {
-          setBreakpoint("md");
-        } else {
-          setBreakpoint("lg");
-        }
-      };
+        setBreakpoint(getBreakpoint(width));
+      }, 200);
 
       handleResize();
-
       window.addEventListener("resize", handleResize);
 
       return () => window.removeEventListener("resize", handleResize);
     }
   }, []);
 
-  return {
-    innerWidth,
-    innerHeight,
-    breakpoint, // Return breakpoint state
-  };
+  return { innerWidth, innerHeight, breakpoint };
 };
 
 export default useMediaQuery;
